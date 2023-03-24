5a99e660434a2640e1705f38f1dde7cbc6adf8dc
Armand Lluka <armandlluka@Armands-MacBook-Pro.local>
2023-03-24 08:30:48 +0100

    addded transcript column

--- app/src/App.vue
+++ app/src/App.vue
@@ -113,41 +113,93 @@ watch(currentParagraph, (newValue) => {
     <CosmosTitle>VideoGenius</CosmosTitle>
     <CosmosInput class="search"></CosmosInput>
   </header>
-  <div class="video-transcript">
-    <VideoPlayer :videoUrl="VIDEO_URL" @timeupdate="handleTimeUpdate" />
-    <Transcript
-      :transcription="transcription"
-      :currentParagraph="currentParagraph"
-    >
-      <template #timestamp="{ paragraphIndex }">
-        <CosmosText
-          @click="jumpToTimestamp(paragraphTimes[paragraphIndex].startTime)"
-          size="x-small"
-          class="timestamp"
-        >
-          {{ paragraphStartTimes[paragraphIndex] }}
-        </CosmosText>
-      </template>
-      <template #words="{ alternative, paragraphIndex }">
-        <CosmosText
-          v-for="(word, index) in alternative.words"
-          :key="index"
-          :kind="currentParagraph === paragraphIndex ? 'normal' : 'subtle'"
-          size="small@medium medium@large"
-        >
-          <span
-            :class="{
+
+  <div class="video-transcript columns">
+    <div class="video-wrapper primary">
+      <VideoPlayer :videoUrl="VIDEO_URL" @timeupdate="handleTimeUpdate" />
+
+    </div>
+    <div class="transcript secondary">
+      <Transcript class="secondary-inner" :transcription="transcription" :currentParagraph="currentParagraph">
+        <template #timestamp="{ paragraphIndex }">
+          <CosmosText @click="jumpToTimestamp(paragraphTimes[paragraphIndex].startTime)" size="x-small" class="timestamp">
+            {{ paragraphStartTimes[paragraphIndex] }}
+          </CosmosText>
+        </template>
+        <template #words="{ alternative, paragraphIndex }">
+          <CosmosText v-if="paragraphStartTimes[paragraphIndex]" v-for="(word, index) in alternative.words" :key="index"
+            :kind="currentParagraph === paragraphIndex ? 'normal' : 'subtle'" size="small@medium medium@small"
+            class="word">
+            <span :class="{
               currentWord:
                 currentParagraph === paragraphIndex &&
                 currentWordIndex === index,
-            }"
-            @click="jumpToWord(word)"
-          >
-            {{ word.word }}
-          </span>
-          <span>&nbsp;</span>
-        </CosmosText>
-      </template>
-    </Transcript>
+            }" @click="jumpToWord(word)">
+              {{ word.word }}
+            </span>
+            <span>&nbsp;</span>
+          </CosmosText>
+        </template>
+      </Transcript>
+    </div>
+
   </div>
 </template>
+
+<style>
+.search {
+  width: 100%
+}
+
+.columns {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  min-width: calc(360px / (16 / 9));
+  max-width: 1000px;
+}
+
+.primary {
+  max-width: 75%;
+  min-width: 600px;
+  margin-left: 24px;
+  padding-top: 24px;
+  padding-right: 24px;
+  min-height: 400px;
+}
+
+.secondary {
+  position: relative;
+  max-width: 50%;
+  min-width: 400px;
+  padding-top: 24px;
+  padding-right: 24px;
+}
+
+.secondary-inner {
+  max-height: 340px;
+  margin-bottom: 24px;
+  overflow: auto;
+  border-radius: 12px;
+  border: 1px solid #e0e0e0;
+  box-sizing: border-box;
+  padding: 0.5rem;
+}
+
+.currentParagraph {
+  background: #f8f8f8;
+}
+
+.currentWord {
+  outline: 2px solid #0a86cb;
+  outline-offset: 2px;
+  border-radius: 5px;
+}
+
+.timestamp {
+  width: 100%;
+  margin-bottom: 0.25rem;
+  --cosmos-text-color: #0a86cb;
+}
+</style>
