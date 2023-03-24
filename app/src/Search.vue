49e37c4a04422c2b6f91efade95791b672ce3813
Armand Lluka <armandlluka@Armands-MacBook-Pro.local>
2023-03-24 08:18:20 +0100

    addded transcript column

---
+++ app/src/Search.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { defineComponent, ref } from "vue";
+import { CosmosText, CosmosInput } from "@cosmos/web/vue";
+
+const search = ref("");
+
+</script>
+
+<template>
+  <div class="search__container">
+    <CosmosInput v-model="search" />
+  </div>
+</template>
+
+<style>
+.search__container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 400px;
+  margin: 40px auto;
+}
+</style>
